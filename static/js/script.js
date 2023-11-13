f4a0240ecb48b4baa8d0d6782c0604c4176ea57d
Lem E. Tweakit <ohm@airmail.cc>
2023-11-13 01:29:32 +0000

    rotate image on click

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,25 @@ window.onload = function() {
     //rotate logo on load
     document.getElementById('logo').classList.add('rotate');
 
+    // Remove class on animation end    
+    logo.addEventListener('animationend', function() {
+        logo.classList.remove('rotate');
+    });
+
+    // Event listener for click
+    logo.addEventListener('click', function() {
+        this.classList.add('rotate');
+
+    // Remove class on animation end    
+        this.addEventListener('animationend', function() {
+            this.classList.remove('rotate');
+        });
+    });
+
     // Set a tags to open in new tab
     var links = document.getElementsByTagName('a');
     for (var i = 0; i < links.length; i++) {
             links[i].target = '_blank';
     }
 };
+
