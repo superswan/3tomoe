ec1898b066b0a7b5f61b9623f1d31859199982da
Lem E. Tweakit <ohm@airmail.cc>
2023-11-13 01:03:43 +0000

    update styling, add links

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
 window.onload = function() {
+    //rotate logo on load
     document.getElementById('logo').classList.add('rotate');
+
+    // Set a tags to open in new tab
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+            links[i].target = '_blank';
+    }
 };
